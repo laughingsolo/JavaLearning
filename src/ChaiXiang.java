f8d637ec4f7f1596621cbf586729847331fdd33b
laughingsolo <306112693@qq.com>
2018-10-08 14:42:47 +0800

    update

--- src/ChaiXiang.java
+++ src/ChaiXiang.java
@@ -1,5 +1,11 @@
 public class ChaiXiang {
     public static void main(String[] args) {
+        int a=1;
+        Integer aa = new Integer(a);//装箱
+        int b=aa.intValue();//拆箱
+        int c=aa;//自动拆箱
+        System.out.println(a+" "+b+" "+c);
+
 
     }
 }
